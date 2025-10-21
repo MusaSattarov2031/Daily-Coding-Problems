06dbe849d19c2e40cf7c2f66e92d45258ea37ad8
Musa <musasattarov855@gmail.com>
2025-10-21 21:47:10 +0300

    Fixing the bag with zeroes, by using left and right products from i. Gonna practise pull

--- 002/mysol2.py
+++ 002/mysol2.py
@@ -1,15 +1,15 @@
 def solution(array):
-    SIZE=len(array)
-    res=[]
-    if SIZE<=1:
-        return res
-    
-    mult=1
-    for i in array:
-        mult=mult*i
-    for i in array:
-        res.append(int(mult/i))
-    return res
+    left=[1]
+    right=[1]
+
+    for i in range(1, len(array)):
+        left.append(left[i-1]*array[i-1])
+        right.append(right[i-1]*array[-i])
+
+    prod=[]
+    for i in range(len(array)):
+        prod.append(left[i]*right[-i-1])
+    return prod
 
 print(solution([1, 2, 3, 4, 5]))
         
--- 002/sol.py
+++ 002/sol.py
@@ -1,10 +1,12 @@
 def productExceptSelf(nums):
     leftProducts = [1]
     rightProducts = [1]
-    for i in range(1, len(nums)):
-        leftProducts.append(nums[i-1]*leftProducts[i-1])
-        rightProducts.append(nums[-i]*rightProducts[i-1])
+    for i in range(1, len(nums)):#for [1, 2, 3, 4, 5]
+        leftProducts.append(nums[i-1]*leftProducts[i-1])#left will be [1, 1, 2, 6, 24, 120]
+        rightProducts.append(nums[-i]*rightProducts[i-1])#right will be [1, 5, 20, 60, 120, 120]
     prods = []
     for i in range(len(nums)):
-        prods.append(leftProducts[i]*rightProducts[-1-i])
+        prods.append(leftProducts[i]*rightProducts[-1-i])# will be [120, 60, 40, 30, 24]
     return prods
+
+print(productExceptSelf([1, 2, 3, 4, 5])==[120, 60, 40, 30, 24])
